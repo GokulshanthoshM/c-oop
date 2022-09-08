f270a1bf8192758c02f9065a4853c6ae8945f2ba
GokulshanthoshM <112715541+GokulshanthoshM@users.noreply.github.com>
2022-09-08 12:58:54 +0530

    Add files via upload

---
+++ eligibilty.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main ()
+{
+   int age;
+   
+   cout<<"Enter your real age: ";
+   cin>>age;
+   if((age >= 'a'&& age <= 'z') || (age >= 'A' && age <= 'Z')) 
+   {
+   	cout<<"enter valid number";
+   }
+   else if(age<0)
+   {
+   	cout<<"Invalid Input";
+   	
+   }
+   else if(age<18)
+   {
+   	cout<<"You are eligible to vote at"<<18-age<<" "<<"years";
+   }
+   else 
+   {
+   	cout<<"You are eligible to vote for dmk";
+   	
+   }
+   return 0;
+}
